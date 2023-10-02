1cdf27833c6ffb30cef83de1b01988c21d3cd154
Red1977 <siobhan_platten@hotmail.com>
2023-10-02 18:28:31 +0100

    actionManager separate function

--- scripts/create_scene.js
+++ scripts/create_scene.js
@@ -1,90 +1,93 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
+const addActionManagerToLabel = function (label){
+        //highlight a mesh
+        const hl = new BABYLON.HighlightLayer("hl1", scene);
+        
+        //Set up mouseover event
+        let actionManager = new BABYLON.ActionManager(scene);
+        label.actionManager = actionManager;
+
+        label.isPickable = true;
+
+        //if cursor is hovering over it,  highlight of the mesh
+        actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function(ev){
+                hl.addMesh(label, BABYLON.Color3.Teal());
+        }));
+        //Remove highlight when cursor no longer over mesh
+        actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function(ev){
+                hl.removeAllMeshes();
+        }));
+ 
+        //recognise click event
+        actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(ev){
+                console.log("mesh clicked");
+                const input = document.createElement('input');
+                input.type = 'file';
+
+                input.onchange = () => {
+                        const files = Array.from(input.files);
+                        const file = files[0];
+
+                        const reader = new FileReader();
+                        reader.onload = function(e) {
+                        const contents = e.target.result;
+
+                        var labelTexture = new BABYLON.Texture(contents, scene);
+                        labelTexture.name = "labelTex";
+                        labelTexture.vScale = -1;
+
+                        label.material.albedoTexture = labelTexture;
+                        label.material.backFaceCulling = true;
+                        };
+                        reader.readAsDataURL(file);
+                };
+                input.click();
+        }));
+            
+        hl.blurHorizontalSize = 1;
+        hl.blurVerticalSize = 1;
+
+}
+
 const createScene = function () {
          // This creates a basic Babylon Scene object (non-mesh)
          const scene = new BABYLON.Scene(engine);
 
          // This creates and positions a free camera (non-mesh)
          const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
- 
-         let container = BABYLON.AssetContainer;
- 
-         BABYLON.SceneLoader.LoadAssetContainer("/assets/", "pump_bottle_label_camera_sphere.glb", scene, function (container) {
+
+         const container = BABYLON.SceneLoader.LoadAssetContainer("/assets/", "pump_bottle_label_camera_sphere.glb", scene, function (container) {
              
              container.addAllToScene();
  
              //scene camera to one loaded
              scene.activeCamera = container.cameras[0];
- 
              scene.meshes = container.meshes;
+
+             label1 = scene.getMeshByName("Label");
+
+             addActionManagerToLabel(label1);
  
-             //highlight a mesh
-             const hl = new BABYLON.HighlightLayer("hl1", scene);
- 
-             //Set up mouseover event
-             let actionManager = new BABYLON.ActionManager(scene);
-             container.meshes[2].actionManager = actionManager;
- 
-             container.meshes[2].isPickable = true;
- 
-             //if cursor is hovering over it,  highlight of the mesh
-             actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function(ev){
-                 hl.addMesh(container.meshes[2], BABYLON.Color3.Teal());
-             }));
-             //Remove highlight when cursor no longer over mesh
-             actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function(ev){
-                 hl.removeAllMeshes();
-             }));
- 
-             //recognise click event
-             actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(ev){
-                 console.log("mesh clicked");
-                 const input = document.createElement('input');
-                 input.type = 'file';
- 
-                 input.onchange = () => {
-                     const files = Array.from(input.files);
-                     const file = files[0];
- 
-                     const reader = new FileReader();
-                     reader.onload = function(e) {
-                         const contents = e.target.result;
- 
-                         var labelTexture = new BABYLON.Texture(contents, scene);
-                         labelTexture.name = "labelTex";
-                         //labelTexture.uScale = -1;
-                         labelTexture.vScale = -1;
-                         
- 
-                         container.meshes[2].material.albedoTexture = labelTexture;
-                         container.meshes[2].material.backFaceCulling = true;
-                     };
-                     reader.readAsDataURL(file);
-                 };
-                 input.click();
-             }));
              
-             hl.blurHorizontalSize = 1;
-             hl.blurVerticalSize = 1;
- 
-             //// GUI
-             var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
- 
-             var button = BABYLON.GUI.Button.CreateImageOnlyButton("but", "https://i.imgur.com/WWzizlq.png");
-             button.width = "100px";
-             button.height = "100px";
-             button.color = "transparent"
-             button.image.detectPointerOnOpaqueOnly = true;
-             button.children[0].detectPointerOnOpaqueOnly = true;
- 
-             button.alpha = 0.5;
-             button.onPointerClickObservable.add(function () {
-                 container.meshes[2].material.albedoTexture.vScale = 1;
-             });
-         
-             advancedTexture.addControl(button);
- 
+                //// GUI
+                var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        
+                var rect1 = new BABYLON.GUI.Rectangle();
+                rect1.width = 0.2;
+                rect1.height = "40px";
+                rect1.cornerRadius = 20;
+                rect1.thickness = 4;
+                rect1.background = "gray";
+                advancedTexture.addControl(rect1);
+
+                rect1.linkWithMesh(label1);   
+                rect1.linkOffsetY = 50;
+                rect1.linkOffsetX = 175;
+                rect1.alpha = 0.5;
+                
+
   
          });
  
